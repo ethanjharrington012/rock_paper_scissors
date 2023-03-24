ca366974e828b56e367873adc909bc427fe0001e
Manja Wilson <manjafiebig@aol.com>
2023-03-24 13:10:45 -0400

    first commit

---
+++ rpc.py
@@ -0,0 +1,17 @@
+# rock_paper_scissors
+
+# 'player'
+# 'computer'
+
+# first action: computer - random pick ['rock', 'paper', 'scissors']
+# second action: player - pick ['rock', 'paper', 'scissors']
+
+# outcome:  -'Game Tied'
+#           -'You Win'
+#           -'You Loose'
+
+# collect input until 'I quit'
+# print 'Thank you for playing!'
+
+def rock_paper_scissors():
+    
